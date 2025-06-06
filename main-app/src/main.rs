02e4c6c67cef19e9201773c504229d8cbb86b8a3
chris-dem <christosdeme@gmail.com>
2025-06-06 15:24:05 +0100

    Adding Stage one

--- main-app/src/main.rs
+++ main-app/src/main.rs
@@ -1,9 +1,13 @@
-use bevy::{input::common_conditions::input_just_pressed, prelude::*};
+use bevy::{
+    ecs::observer::TriggerTargets, input::common_conditions::input_just_pressed, prelude::*,
+};
 
-const D_RADIUS: f32 = 50.0f32;
-const BBDIMS: (f32, f32) = (50.0f32, 50.0f32);
+// const D_RADIUS: f32 = 50.0f32;
 const VCOLOUR: Color = Color::srgb_u8(47, 158, 68);
 const GCOLOUR: Color = Color::srgb_u8(103, 65, 217);
+const MARGIN: Val = Val::Px(12.);
+static VALTEST: &str = "Value Mode";
+static GATETEST: &str = "Gate Mode";
 
 #[derive(Debug, Clone, Copy)]
 enum CurrentChoice {
@@ -26,30 +30,25 @@ struct GateMode(CurrentChoice);
 #[derive(Component)]
 struct ColourPicker;
 
-#[derive(Resource)]
-struct ValueResource(pub Handle<ColorMaterial>);
-
-#[derive(Resource)]
-struct GateResource(pub Handle<ColorMaterial>);
-
 // Add necessary shapes
-fn setup(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
-) {
-    let rect = meshes.add(Rectangle::new(BBDIMS.0, BBDIMS.1));
+fn setup(mut commands: Commands) {
     commands.spawn(Camera2d);
-    let hand_v = materials.add(VCOLOUR);
-    materials.add(GCOLOUR);
-    commands.spawn((
-        Mesh2d(rect),
-        MeshMaterial2d(hand_v),
-        Transform::default(),
-        ColourPicker,
-    ));
 
     commands.insert_resource(GateMode(CurrentChoice::Value));
+    let bund = spawn_nested_text_bundle(&mut commands, VCOLOUR, UiRect::all(Val::Px(5.)), VALTEST);
+    commands.entity(bund).insert(ColourPicker);
+    commands
+        .spawn(Node {
+            // fill the entire window
+            width: Val::Percent(100.),
+            height: Val::Percent(100.),
+            flex_direction: FlexDirection::Column,
+            align_items: AlignItems::Start,
+            padding: UiRect::all(MARGIN),
+            row_gap: MARGIN,
+            ..Default::default()
+        })
+        .add_child(bund);
 }
 
 fn toggle_status(mut query: ResMut<GateMode>) {
@@ -57,16 +56,29 @@ fn toggle_status(mut query: ResMut<GateMode>) {
 }
 
 fn draw_col(
-    mat: Single<&MeshMaterial2d<ColorMaterial>, With<ColourPicker>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
+    mut mat: Single<(&mut BackgroundColor, &mut Children), With<ColourPicker>>,
+    mut query_text: Query<&mut Text>,
     state: Res<GateMode>,
 ) {
-    let m = materials
-        .get_mut(&mat.0)
-        .expect("No clue wtf this is doing");
-    match &state.0 {
-        CurrentChoice::Gate => *m = GCOLOUR.into(),
-        CurrentChoice::Value => *m = VCOLOUR.into(),
+    let (ref mut bg, ref mut children) = *mat;
+    let text_children = query_text.get_mut(
+        children
+            .entities()
+            .next()
+            .expect("Should be loaded with text"),
+    );
+    if let Ok(mut value) = text_children {
+        match &state.0 {
+            CurrentChoice::Gate => {
+                bg.0 = GCOLOUR;
+                value.0 = GATETEST.into();
+            }
+
+            CurrentChoice::Value => {
+                bg.0 = VCOLOUR;
+                value.0 = VALTEST.into();
+            }
+        }
     }
 }
 
@@ -80,4 +92,25 @@ fn main() {
         )
         .add_systems(Update, draw_col)
         .run();
+}
+
+fn spawn_nested_text_bundle(
+    builder: &mut Commands,
+    background_color: Color,
+    margin: UiRect,
+    text: &str,
+) -> Entity {
+    builder
+        .spawn((
+            Node {
+                margin,
+                padding: UiRect::axes(Val::Px(5.), Val::Px(1.)),
+                ..default()
+            },
+            BackgroundColor(background_color),
+        ))
+        .with_children(|builder| {
+            builder.spawn((Text::new(text), TextFont::default(), TextColor::BLACK));
+        })
+        .id()
 }
